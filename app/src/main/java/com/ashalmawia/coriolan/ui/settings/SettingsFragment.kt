36f9b482b43b26aca7d4dc0d0ab58d8e72c8cef4
ashalmawia <ashalmawia.it@gmail.com>
2019-06-16 23:23:48 +0200

    [Settings] Add version info

--- app/src/main/java/com/ashalmawia/coriolan/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/ashalmawia/coriolan/ui/settings/SettingsFragment.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.ashalmawia.coriolan.BuildConfig
 import com.ashalmawia.coriolan.R
 import com.ashalmawia.coriolan.data.prefs.Preferences
 import com.takisoft.fix.support.v7.preference.EditTextPreference
@@ -17,6 +18,7 @@ import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompatDividers
 private const val PREFERENCE_CARD_TYPES = "card_types"
 private const val PREFERENCE_DAILY_LIMITS_NEW = "daily_limit_new_cards"
 private const val PREFERENCE_DAILY_LIMITS_REVIEW = "daily_limit_review_cards"
+private const val PREFERENCE_VERSION = "app_version"
 
 class SettingsFragment : PreferenceFragmentCompatDividers() {
 
@@ -40,6 +42,11 @@ class SettingsFragment : PreferenceFragmentCompatDividers() {
 
         setUpCardTypes()
         setUpDailyLimits()
+        setUpVersionInfo()
+    }
+
+    private fun setUpVersionInfo() {
+        findPreference(PREFERENCE_VERSION).summary = BuildConfig.VERSION_NAME
     }
 
     private fun setUpDailyLimits() {
@@ -72,13 +79,13 @@ class SettingsFragment : PreferenceFragmentCompatDividers() {
         return try {
             val int = value.toInt()
             if (int < 0) {
-                showMessage("Number incorrect")
+                showMessage(getString(R.string.settings__error_number_negative, value))
                 false
             } else {
                 true
             }
         } catch (e: NumberFormatException) {
-            showMessage("Number incorrect")
+            showMessage(getString(R.string.settings__error_number_incorrect, value))
             false
         }
     }
@@ -91,9 +98,9 @@ class SettingsFragment : PreferenceFragmentCompatDividers() {
         try {
             return super.onCreateView(inflater, container, savedInstanceState)
         } finally {
-            setDividerPreferences(PreferenceFragmentCompatDividers.DIVIDER_PADDING_CHILD
-                    or PreferenceFragmentCompatDividers.DIVIDER_CATEGORY_AFTER_LAST
-                    or PreferenceFragmentCompatDividers.DIVIDER_CATEGORY_BETWEEN)
+            setDividerPreferences(DIVIDER_PADDING_CHILD
+                    or DIVIDER_CATEGORY_AFTER_LAST
+                    or DIVIDER_CATEGORY_BETWEEN)
         }
     }
 }
