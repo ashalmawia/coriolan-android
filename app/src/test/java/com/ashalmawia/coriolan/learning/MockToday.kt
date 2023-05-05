d20257595d2454ef1af64a8569efd62d91b7368a
ashalmawia <ashalmawia.it@gmail.com>
2023-05-06 00:25:13 +0200

    Fix: tests are failing between midnight and 4am

--- app/src/test/java/com/ashalmawia/coriolan/learning/MockToday.kt
+++ app/src/test/java/com/ashalmawia/coriolan/learning/MockToday.kt
@@ -1,6 +1,3 @@
 package com.ashalmawia.coriolan.learning
 
-import org.joda.time.LocalDate
-import org.joda.time.LocalTime
-
-fun mockToday(): LearningDay = LocalDate.now().toDateTime(LocalTime(4, 0))
+fun mockToday(): LearningDay = TodayManager.today()
