d4df98f0f6c0ca3d2220d9cd9cd2b6af3fddcfd5
ashalmawia <ashalmawia.it@gmail.com>
2023-05-06 00:25:04 +0200

    Store date added information for every Card

--- app/src/main/java/com/ashalmawia/coriolan/data/storage/sqlite/payload/CardPayload.kt
+++ app/src/main/java/com/ashalmawia/coriolan/data/storage/sqlite/payload/CardPayload.kt
@@ -1,5 +1,8 @@
 package com.ashalmawia.coriolan.data.storage.sqlite.payload
 
-data class CardPayload(val translationIds: List<TermId>)
+import com.ashalmawia.coriolan.util.timespamp
+import org.joda.time.DateTime
+
+data class CardPayload(val translationIds: List<TermId>, val dateAdded: Long = DateTime.now().timespamp)
 
 data class TermId(val id: Long)
